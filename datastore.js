e9407d05ba891845ddf6df48d024714ae7851471
Eric Feldman <ericfeldman2@gmail.com>
2019-06-02 20:05:22 -0400

    first commit for final draft of quiz app

---
+++ datastore.js
@@ -0,0 +1,114 @@
+const STORE = [
+// Question 1
+    {
+    question: 'Which of the following is NOT one of the accepted opinions regarding how long you have to wait between having dairy and having meat?',
+    answers: [
+    '1 Hour', 
+    '3 Hours', 
+    '6 Hours', 
+    '9 Hours'
+    ],
+    correctAnswer: '9 Hours'
+},
+// Question 2
+{
+    question: 'Which of the following is an example of a dairy item for which you would have to wait an extended period of time before consuming meat (According to the Rema)?',
+    answers: [
+    'Oreo Milkshake', 
+    'Parmesan Cheese', 
+    'Greek Yogurt', 
+    'Chocolate Milk'
+    ],
+    correctAnswer: 'Parmesan Cheese'
+},
+// Question 3
+{
+    question: 'What is the name for the halachic concept referring to secondary flavor transfer?',
+    answers: [
+    'Nat Bar Nat', 
+    'Notayn Tam Lifgam', 
+    'Aino Ben Yomo', 
+    'Shimon Bar Yochai'
+    ],
+    correctAnswer: 'Nat Bar Nat'
+},
+// Question 4
+{
+    question: 'What is one unique aspect of sharp foods, such as garlic and onions?',
+    answers: [
+    'Sharp foods are all considered to be meat', 
+    'A knife that cuts a sharp food can transfer flavor, even without heat', 
+    'Sharp foods need to sit out for 24 hours before consumption', 
+    'Sharp foods can transfer flavor just through physical contact'
+    ],
+    correctAnswer: 'A knife that cuts a sharp food can transfer flavor, even without heat'
+},
+// Question 5
+{
+    question: 'The Rema’s practice of ne’itzah, which is typically not done practically, refers to what method of kashering knives?',
+    answers: [
+    'Using a blowtorch', 
+    'Sticking the knife in the ground in various places', 
+    'Pouring boiling water over the knife', 
+    'Immersing the knife in a mikvah'
+    ],
+    correctAnswer: 'Sticking the knife in the ground in various places'
+},
+// Question 6
+{
+    question: 'Scenario: Parve noodles were cooked in a meat pot, and they were accidentally stirred with a dairy spoon.  How would the Rema and the Mechaber differ in their ruling with respect to the status of the food, pot, and spoon?  Assume that everything was clean and had been used within 24 hours.',
+    answers: [
+    'Mechaber - food is okay, pot and spoon need kashering vs. Rema - all permitted', 
+    'Rema - food is okay, pot and spoon need kashering vs. Mechaber - all permitted', 
+    'Mechaber - all prohibited / needs kashering, vs. Rema - all permitted', 
+    'Rema - all prohibited / needs kashering, vs. Mechaber - all permitted'
+    ],
+    correctAnswer: 'Rema - all prohibited / needs kashering, vs. Mechaber - all permitted'
+},
+// Question 7
+{
+    question: 'If someone accidentally cooks milk in a meat pot and meat spoon, which of the following is correct about the outcome?  Assume nothing had been used within 24 hours.',
+    answers: [
+    'The food cannot be eaten and the spoon and pot need kashering', 
+    'The spoon and pot needs kashering, but the food is fine to eat', 
+    'Nothing needs kashering and the food is fine to eat', 
+    'The person has broken the law and the authorities need to be notified'
+    ],
+    correctAnswer: 'The spoon and pot needs kashering, but the food is fine to eat'
+},
+// Question 8
+{
+    question: 'Which of the following is items definitely requires tevila (immersion in a mikvah) with a bracha?',
+    answers: [
+    'Metal Wine Opener', 
+    'Disposable foil pan', 
+    'Glass jar for storage', 
+    'Stainless steel pot'
+    ],
+    correctAnswer: 'Stainless steel pot'
+},
+// Question 9
+{
+    question: '',
+    answers: [
+    '', 
+    '', 
+    '', 
+    ''
+    ],
+    correctAnswer: ''
+},
+// Question 10
+{
+    question: '',
+    answers: [
+    '', 
+    '', 
+    '', 
+    ''
+    ],
+    correctAnswer: ''
+},
+
+    
+]
---
+++ index.js
@@ -0,0 +1,48 @@
+// questions begin at 0
+const questionNumber = 0
+
+// score begins at 0
+const score = 0
+
+// generate question html
+function generateQuestion() {
+    if questionCount < STORE.length) {
+        return 
+    }
+}
+
+// increment question number
+function updateQuestionNumber() {
+    questionNumber++;
+    $(.questionCount).text(questionNumber+1)
+}
+// increment score
+function updateScore() {
+    score++;
+    $(.scoreCount).text(score)
+}
+// start quiz
+
+// render question in DOM
+
+// user selects answer
+
+// user feedback for correct answer
+
+// user feedback for incorrect answer
+
+// update score text
+
+// html for when the quiz is over
+
+// what happens when the user clicks next
+
+// reload page to re-take quiz
+
+// run quiz functions
+
+
+
+
+
+
