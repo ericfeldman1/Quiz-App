5b694bab717dab168f647264ba07a2392c6e8e6f
Eric Feldman <ericfeldman2@gmail.com>
2019-06-03 07:07:34 -0400

    updated JS and html a bit

--- index.js
+++ index.js
@@ -4,24 +4,61 @@ const questionNumber = 0
 // score begins at 0
 const score = 0
 
-// generate question html
+// generate question - html code
 function generateQuestion() {
     if questionCount < STORE.length) {
         return 
+        <div class="question-${questionNumber}">
+        <h2>${STORE[questionNumber].question}</h2>
+        <form class="questionForm" action="URL" method="post">
+        <fieldset name ="multiple-choice-options">
+        <label class="answerOption">
+            <input type="radio" 
+            value="${STORE[questionNumber].answers[0]}" 
+            name="answerOption" required>
+            <span>${STORE[questionNumber].answers[0]}</span>    
+        </label>
+        <label class="answerOption">
+            <input type="radio" 
+            value="${STORE[questionNumber].answers[1]}" 
+            name="answerOption" required>
+            <span>${STORE[questionNumber].answers[1]}</span>    
+        </label>
+        <label class="answerOption">
+            <input type="radio" 
+            value="${STORE[questionNumber].answers[2]}" 
+            name="answerOption" required>
+            <span>${STORE[questionNumber].answers[2]}</span>    
+        </label>
+        <label class="answerOption">
+            <input type="radio" 
+            value="${STORE[questionNumber].answers[3]}" 
+            name="answerOption" required>
+            <span>${STORE[questionNumber].answers[3]}</span>    
+        </label>
+        <button type="Submit">Submit</button>
+        </fieldset>
+        </form>
+    }
+    else {
+    //    Still need to create these functions
+       renderResults();
+        restartQuiz()
+        $(.questionNumber).text(10)
     }
 }
 
-// increment question number
+{/* increment question number */}
 function updateQuestionNumber() {
     questionNumber++;
-    $(.questionCount).text(questionNumber+1)
+    $(.questionNumber).text(questionNumber+1)
 }
-// increment score
+{/* increment score */}
 function updateScore() {
     score++;
     $(.scoreCount).text(score)
 }
-// start quiz
+start quiz
 
 // render question in DOM
 
